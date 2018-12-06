2ab55b8dccd996299ff17837853ef6f466d78465
Nick Kramer <krames12@gmail.com>
2018-12-05 21:08:40 -0500

    created first component, also created mock jobs data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <JobList />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import JobList from './components/JobList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    JobList
   }
 }
 </script>
---
+++ src/assets/jobs.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    name: "dragon",
+    qty: 3,
+    color: "red"
+  },
+  {
+    name: "pi case",
+    qty: 8,
+    color: "black"
+  },
+  {
+    name: "mario",
+    qty: 1,
+    color: "grey"
+  }
+];
