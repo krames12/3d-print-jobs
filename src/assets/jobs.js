3cece3bcc18eef9c892df5a19a4a5eb824061cfa
Nick Kramer <krames12@gmail.com>
2018-12-05 21:28:30 -0500

    removed logo, added id to jobs

--- src/assets/jobs.js
+++ src/assets/jobs.js
@@ -1,17 +1,20 @@
 export default [
   {
+    id: 1,
     name: "dragon",
     qty: 3,
     completed: 2,
     color: "red"
   },
   {
+    id: 2,
     name: "pi case",
     qty: 8,
     completed: 4,
     color: "black"
   },
   {
+    id: 3,
     name: "mario",
     qty: 1,
     completed: 0,
