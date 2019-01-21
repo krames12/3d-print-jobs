23ce373f190af97f1b960c6c7fdf34db12e76988
Nick Kramer <nkramer@cwims.com>
2019-01-21 09:58:10 -0500

    remove full firebase package, imported sub-packages to reduce build size

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-import { initializeApp, database, auth } from "firebase";
+import { firebase } from "@firebase/app";
+import { database } from "@firebase/database";
+import { auth } from "@firebase/auth";
 
 let firebaseApiKey =
   process.env.VUE_APP_FIREBASE_API_KEY || process.env.FIREBASE_API_KEY;
@@ -11,9 +13,9 @@ const config = {
   projectId: "kramer-print-jobs"
 };
 
-initializeApp(config);
+firebase.initializeApp(config);
 
 export default {
-  database: database(),
-  auth: auth()
+  database: database,
+  auth: auth
 };
